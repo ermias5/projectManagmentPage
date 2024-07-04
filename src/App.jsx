91f695e25b471b43a79bdf4a5b62db16398aed98
ermias alehegn <ermiaseat1987@gmail.com>
2024-07-04 14:28:41 +0300

    creating and addng projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./index.css";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+import Header from "./components/Header";
+// import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import NewProject from "./components/NewProject";
+import { useState } from "react";
 
 export default function App() {
+  const [hasProject, setHasProject] = useState(false);
+
+  function handleProjectSelection() {
+    setHasProject(true);
+  }
   return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
-    </main>
+    <>
+      <header className="text-blue-800 text-center text-3xl">
+        <Header />
+      </header>
+      <main className="h-screen my-8 flex gap-8">
+        <ProjectsSidebar handleProjectSelect={handleProjectSelection} />
+        {hasProject ? <NewProject /> : <NoProjectSelected />}
+        {/* <NewProject /> */}
+      </main>
+    </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1 className="">Project Managment Dashboard</h1>
+    </div>
+  );
+}
